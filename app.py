7ed4b8b8b26a26d75d60b1a7a84d59042ac8c391
BrookeNeupert <brne5817@colorado.edu>
2023-11-03 17:19:15 +0000

    created new DB route

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn.close()
     return "Database Connection Successful"
 
+@app.route('db_create')
+def create_db():
+    conn = psycopg2.connect("postgres://lab10_db_il0a_user:93JffJXJ94OrzLPAK7v5CvAp1GsGS7Si@dpg-cl2iaubmgg9c73aul1l0-a/lab10_db_il0a")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Created"
+
